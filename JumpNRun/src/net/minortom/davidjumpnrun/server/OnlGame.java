4fbd6cedfb89a3c9ac00e4960210b7cbd977c6af
MinorTom <donotwishtoshare>
2019-03-28 21:09:09 +0100

    changes in server

--- JumpNRun/src/net/minortom/davidjumpnrun/server/OnlGame.java
+++ JumpNRun/src/net/minortom/davidjumpnrun/server/OnlGame.java
@@ -4,12 +4,40 @@
  */
 package net.minortom.davidjumpnrun.server;
 
+import java.util.HashMap;
+import jumpnrun.JumpNRun;
+
 public class OnlGame {
     
-    public Server server;
+    Server server;
     
     public String gameName;
+    public int playersMax;
+    public JumpNRun.Gamemode gamemode;
+    public double timeLimit;
+    public int respawnLimit;
+    
+    public String mapName;
+    public String mapText;
+    
+    public HashMap<String,String> players; // Id, skin
     
     
     
+    public OnlGame(Server server, String gameName, int playersMax, JumpNRun.Gamemode gamemode, double timeLimit, int respawnLimit, String mapName, String playerOneId, String playerOneSkin){
+        this.server = server;
+        
+        this.gameName = gameName;
+        this.playersMax = playersMax;
+        this.gamemode = gamemode;
+        this.timeLimit = timeLimit;
+        this.respawnLimit = respawnLimit;
+        this.mapName = mapName;
+        
+        mapText = MapHelper.getMap(mapName);
+        
+        players = new HashMap<>();
+        
+        players.put(playerOneId, playerOneSkin);
+    }
 }
