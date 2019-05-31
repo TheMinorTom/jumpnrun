1c59055b4fc3e4e682d384a574dbd49835c68073
MinorTom <donotwishtoshare>
2019-05-31 13:35:05 +0200

    remove tompkg

--- TomPKG/src/main/java/net/minortom/tompkg/pkgtypes/Package.java
+++
@@ -1,9 +0,0 @@
-/*
- * This File is licensed under the MIT License. You should have already received a copy located at LICENSE.txt
- * Copyright 2019 MinorTom <mail in license file>
- */
-package net.minortom.tompkg.pkgtypes;
-
-public interface Package {
-    
-}
